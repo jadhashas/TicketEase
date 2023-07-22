5d369e23ea76fcbb8485631e2fd474e58fa557c3
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-07-22 13:30:13 +0100

    Upload files (#2)
    * Actor classe
    
    * ajouter les autres classes

---
+++ TicketEase/Data/Enums/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketEase.Data.Enums
+{
+    /*
+     * Une énumération (enum) :
+     * en C# est un type de données qui permet de définir un ensemble de valeurs nommées constantes. Une énumération permet de créer un groupe de valeurs associées, 
+     * facilitant ainsi leur utilisation dans le code et rendant le code plus lisible et maintenable.
+     */
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
