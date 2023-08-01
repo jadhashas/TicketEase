4b9c0cb9f690a6727e541e1e818e4735be027208
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-08-01 13:59:12 +0100

    nav

--- TicketEase/Controllers/MoviesController.cs
+++ TicketEase/Controllers/MoviesController.cs
@@ -14,7 +14,7 @@ namespace TicketEase.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var AllMovies = await _context.Movies.ToListAsync();
+            var AllMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(AllMovies);
         }
     }
--- TicketEase/wwwroot/js/site.js
+++ TicketEase/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const managementLink = document.getElementById('navbarDropdown');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 
-// Write your JavaScript code.
+// Track the state of the dropdown menu
+let isDropdownOpen = false;
+
+// Function to toggle the dropdown menu
+function toggleDropdown() {
+    if (isDropdownOpen) {
+        // Slide up the dropdown to hide it
+        $(dropdownMenu).slideUp(200);
+    } else {
+        // Slide down the dropdown to show it
+        $(dropdownMenu).slideDown(200);
+    }
+    isDropdownOpen = !isDropdownOpen;
+}
+
+// Add click event listener to the management link
+managementLink.addEventListener('click', toggleDropdown);
+
+// Add click event listener to the dropdown items to close the dropdown when clicked
+const dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item');
+dropdownItems.forEach(item => {
+    item.addEventListener('click', () => {
+        $(dropdownMenu).slideUp(200);
+        isDropdownOpen = false;
+    });
+});
