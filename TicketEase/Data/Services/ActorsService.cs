200fabd3f5c23ead7992ab0df66cf8be57d86080
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-08-06 22:57:03 +0100

    CRUD

--- TicketEase/Data/Services/ActorsService.cs
+++ TicketEase/Data/Services/ActorsService.cs
@@ -16,24 +16,29 @@ namespace TicketEase.Data.Services
 			await _context.SaveChangesAsync();
 		}
 
-		public void Delete(int id)
+		public async Task DeleteAsync(int id)
 		{
-			throw new NotImplementedException();
-		}
-		// async
-		public async Task<IEnumerable<Actor>> GetAll()
+            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            _context.Actors.Remove(result);
+			await _context.SaveChangesAsync();
+        }
+        // async
+        public async Task<IEnumerable<Actor>> GetAllAsync()
 		{
 			return await _context.Actors.ToListAsync();
 		}
 		
-		public Actor GetById(int id)
+		public async Task<Actor> GetByIdAsync(int id)
 		{
-			throw new NotImplementedException();
+			var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+			return result;
 		}
 
-		public Actor Update(int id, Actor actor)
+		public async Task<Actor> UpdateAsync(int id, Actor newActor)
 		{
-			throw new NotImplementedException();
+			_context.Update(newActor);
+			await _context.SaveChangesAsync();
+			return newActor;
 		}
     }
 }
