ac5453160feffd8b2bf9bf57f8cb3a211acf3938
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-08-07 14:04:56 +0100

    Upload files (#23)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository

---
+++ TicketEase/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TicketEase.Models;
+
+namespace TicketEase.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(T t) => await _context.Set<T>().AddAsync(t);
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry(entity);
+            entityEntry.State = EntityState.Deleted;
+        }
+
+        // async
+        public async Task<IEnumerable<T>> GetAllAsync() =>  await _context.Set<T>().ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+
+        public async Task UpdateAsync(int id, T t)
+        {
+            EntityEntry entityEntry = _context.Entry(t);
+            entityEntry.State = EntityState.Modified;
+        }
+    }
+}
