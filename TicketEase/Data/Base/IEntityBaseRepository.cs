93f84d81ee6b016daeb72a9f55ac28840dc23b5c
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-08-07 14:04:08 +0100

    base repository

---
+++ TicketEase/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using TicketEase.Models;
+
+namespace TicketEase.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T t);
+
+        Task UpdateAsync(int id, T t);
+
+        Task DeleteAsync(int id);
+    }
+}
