685cefb93b5e401a0261eed35922561675eea9ce
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-09-08 22:02:01 +0100

    Upload files (#29)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository
    
    * configurer BaseRepository
    
    * ProducersService configuration with EntityBaseRepository
    
    * Producers
    
    * Last of producer configuration
    
    * Create cinemas configuration
    
    * cinemas configuration

--- TicketEase/Controllers/CinemasController.cs
+++ TicketEase/Controllers/CinemasController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TicketEase.Data;
 using TicketEase.Data.Services;
@@ -32,6 +32,46 @@ namespace TicketEase.Controllers
         {
             if (!ModelState.IsValid) return View(cinema);
             await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET : Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails=await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        // GET : Cinemas/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));   
+        }
+
+        // GET : Cinemas/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cinamsDetails = await _service.GetByIdAsync(id);
+            if (cinamsDetails == null) return View("NotFound");
+            await _service.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }
