d2b0d7cc779144941018b3d09201ef4092736bd1
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-09-08 14:45:50 +0100

    Upload files (#28)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository
    
    * configurer BaseRepository
    
    * ProducersService configuration with EntityBaseRepository
    
    * Producers
    
    * Last of producer configuration
    
    * Create cinemas configuration

---
+++ TicketEase/Data/Services/CinemasService.cs
@@ -0,0 +1,10 @@
+﻿using TicketEase.Data.Base;
+using TicketEase.Models;
+
+namespace TicketEase.Data.Services
+{
+    public class CinemasService:EntityBaseRepository<Cinema>, ICinemasService
+    {
+        public CinemasService(AppDbContext context) : base(context) { }
+    }
+}
