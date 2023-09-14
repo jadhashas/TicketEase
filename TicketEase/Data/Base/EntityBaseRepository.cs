940f9f2fcf141307dd70834d01b0a5333b7e2ccb
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-09-14 14:01:41 +0100

    debut movies

--- TicketEase/Data/Base/EntityBaseRepository.cs
+++ TicketEase/Data/Base/EntityBaseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
 using TicketEase.Models;
 
 namespace TicketEase.Data.Base
@@ -27,6 +28,13 @@ namespace TicketEase.Data.Base
 
         // async
         public async Task<IEnumerable<T>> GetAllAsync() =>  await _context.Set<T>().ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return await query.ToListAsync();
+        }
 
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
--- TicketEase/Data/Base/IEntityBaseRepository.cs
+++ TicketEase/Data/Base/IEntityBaseRepository.cs
@@ -1,10 +1,12 @@
-﻿using TicketEase.Models;
+﻿using System.Linq.Expressions;
+using TicketEase.Models;
 
 namespace TicketEase.Data.Base
 {
     public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
         Task<T> GetByIdAsync(int id);
         Task AddAsync(T t);
 
