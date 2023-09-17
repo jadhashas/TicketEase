1f3e952300b5c4e2b3aeb34401ba1151be1ddb90
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-09-17 15:41:49 +0100

    Setting for the ShoppingCart

---
+++ TicketEase/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketEase.Models;
+
+namespace TicketEase.Data.Cart
+{
+	public class ShoppingCart
+	{
+        public AppDbContext _context { get; set; }
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+		public List<ShoppingCartItem> GetShoppingCartItems()
+		{
+			// Vérifie si la liste ShoppingCartItems est déjà chargée
+			// Si ce n'est pas le cas, chargez-la depuis la base de données
+			return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+				.Where(n => n.ShoppingCartId == ShoppingCartId) // Sélectionne les éléments du panier pour l'utilisateur actuel
+				.Include(n => n.Movie) // Inclut également les données des films associés
+				.ToList()); // Renvoie la liste des éléments du panier
+
+			/*SELECT ShoppingCartItems.*, Movies.*
+			FROM ShoppingCartItems
+			INNER JOIN Movies ON ShoppingCartItems.MovieId = Movies.Id
+			WHERE ShoppingCartItems.ShoppingCartId = [votre valeur ShoppingCartId]
+			*/
+		}
+
+		public double GetShoppingCartTotal()
+		{
+			// Calcule le total du panier en multipliant le prix de chaque élément par sa quantité et en additionnant le tout
+			return _context.ShoppingCartItems
+				.Where(n => n.ShoppingCartId == ShoppingCartId) // Sélectionne les éléments du panier pour l'utilisateur actuel
+				.Select(n => n.Movie.Price * n.Amount) // Calcule le prix total pour chaque élément (prix du film * quantité)
+				.Sum(); // Additionne tous les prix totaux pour obtenir le total du panier
+
+			/*
+			SELECT SUM(Movies.Price * ShoppingCartItems.Amount)
+			FROM ShoppingCartItems
+			INNER JOIN Movies ON ShoppingCartItems.MovieId = Movies.Id
+			WHERE ShoppingCartItems.ShoppingCartId = [votre valeur ShoppingCartId]
+			*/
+		}
+
+	}
+}
