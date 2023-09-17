5949ca4a89b99a9085678542760a2fb47ec54def
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-09-17 16:05:02 +0100

    Upload files (#44)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository
    
    * configurer BaseRepository
    
    * ProducersService configuration with EntityBaseRepository
    
    * Producers
    
    * Last of producer configuration
    
    * Create cinemas configuration
    
    * cinemas configuration
    
    * debut movies
    
    * movie details
    
    * movie details
    
    * Create movies
    
    * configure dorpDowns
    
    * Partial View for button CreateItem
    
    * tt
    
    * Adding new movie to database
    
    * updating movie
    
    * Searching for movie
    
    * configure models : order and orderItems
    
    * Shoppinrg cart item in folder model
    
    * Setting for the ShoppingCart
    
    * Add methode : AddItemToCart
    
    * add methode : RemoveItemfromCart
    
    * Adding OrdersController file

---
+++ TicketEase/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using TicketEase.Data.Cart;
+
+namespace TicketEase.Data.ViewModels
+{
+	public class ShoppingCartVM
+	{
+        public  ShoppingCart ShoppingCart { get; set; }
+        public  double ShoppingCartTotal { get; set; }
+    }
+}
