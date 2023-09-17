195478852bab13ec7ba6ea4300b45883ec303748
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-09-17 15:52:27 +0100

    Add methode : AddItemToCart

--- TicketEase/Data/Cart/ShoppingCart.cs
+++ TicketEase/Data/Cart/ShoppingCart.cs
@@ -13,6 +13,28 @@ namespace TicketEase.Data.Cart
             _context = context;
         }
 
+		public void AddItemToCart(Movie movie)
+		{
+			var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+			if(shoppingCartItem == null)
+			{
+				shoppingCartItem = new ShoppingCartItem()
+				{
+					ShoppingCartId = ShoppingCartId,
+					Movie = movie,
+					Amount = 1,
+				};
+				_context.ShoppingCartItems.Add(shoppingCartItem);
+			}
+			else
+			{
+				shoppingCartItem.Amount++;
+			}
+			_context.SaveChanges();
+		}
+
+
+
 		public List<ShoppingCartItem> GetShoppingCartItems()
 		{
 			// Vérifie si la liste ShoppingCartItems est déjà chargée
