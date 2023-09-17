ec48b6a8c68f5667053792583922af39dd64d048
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-09-17 13:58:33 +0100

    Upload files (#39)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository
    
    * configurer BaseRepository
    
    * ProducersService configuration with EntityBaseRepository
    
    * Producers
    
    * Last of producer configuration
    
    * Create cinemas configuration
    
    * cinemas configuration
    
    * debut movies
    
    * movie details
    
    * movie details
    
    * Create movies
    
    * configure dorpDowns
    
    * Partial View for button CreateItem
    
    * tt
    
    * Adding new movie to database
    
    * updating movie
    
    * Searching for movie
    
    * configure models : order and orderItems

--- TicketEase/Data/AppDbContext.cs
+++ TicketEase/Data/AppDbContext.cs
@@ -12,6 +12,10 @@ namespace TicketEase.Data
         public DbSet<Producer> Producers { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
 
+        // Orders related tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
