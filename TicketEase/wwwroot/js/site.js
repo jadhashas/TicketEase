cf48b1b36aa7d1f944b4d12f2006f7a178e6eba6
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-09-18 23:52:45 +0100

    conclure for order configuration

--- TicketEase/wwwroot/js/site.js
+++ TicketEase/wwwroot/js/site.js
@@ -1,29 +1,56 @@
-﻿const managementLink = document.getElementById('navbarDropdown');
-const dropdownMenu = document.querySelector('.dropdown-menu');
+﻿const profileDropdownToggle = document.querySelector('.profile-dropdown .dropdown-toggle');
+const managementDropdownToggle = document.querySelector('.management-dropdown .dropdown-toggle');
+const profileDropdownMenu = document.querySelector('.profile-dropdown .dropdown-menu');
+const managementDropdownMenu = document.querySelector('.management-dropdown .dropdown-menu');
 
-// Track the state of the dropdown menu
-let isDropdownOpen = false;
+// Track the state of the dropdown menus
+let isProfileDropdownOpen = false;
+let isManagementDropdownOpen = false;
 
-// Function to toggle the dropdown menu
-function toggleDropdown() {
-    if (isDropdownOpen) {
-        // Slide up the dropdown to hide it
-        $(dropdownMenu).slideUp(200);
+// Function to toggle the profile dropdown menu
+function toggleProfileDropdown() {
+    if (isProfileDropdownOpen) {
+        // Slide up the profile dropdown to hide it
+        $(profileDropdownMenu).slideUp(200);
     } else {
-        // Slide down the dropdown to show it
-        $(dropdownMenu).slideDown(200);
+        // Slide down the profile dropdown to show it
+        $(profileDropdownMenu).slideDown(200);
     }
-    isDropdownOpen = !isDropdownOpen;
+    isProfileDropdownOpen = !isProfileDropdownOpen;
 }
 
-// Add click event listener to the management link
-managementLink.addEventListener('click', toggleDropdown);
+// Function to toggle the management dropdown menu
+function toggleManagementDropdown() {
+    if (isManagementDropdownOpen) {
+        // Slide up the management dropdown to hide it
+        $(managementDropdownMenu).slideUp(200);
+    } else {
+        // Slide down the management dropdown to show it
+        $(managementDropdownMenu).slideDown(200);
+    }
+    isManagementDropdownOpen = !isManagementDropdownOpen;
+}
 
-// Add click event listener to the dropdown items to close the dropdown when clicked
-const dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item');
-dropdownItems.forEach(item => {
-    item.addEventListener('click', () => {
-        $(dropdownMenu).slideUp(200);
-        isDropdownOpen = false;
-    });
+// Add click event listener to the profile dropdown toggle
+profileDropdownToggle.addEventListener('click', (e) => {
+    toggleProfileDropdown();
+    e.stopPropagation(); // Prevent the event from propagating to document
+});
+
+// Add click event listener to the management dropdown toggle
+managementDropdownToggle.addEventListener('click', (e) => {
+    toggleManagementDropdown();
+    e.stopPropagation(); // Prevent the event from propagating to document
+});
+
+// Add click event listener to the document to close the dropdowns when clicking outside
+document.addEventListener('click', () => {
+    if (isProfileDropdownOpen) {
+        $(profileDropdownMenu).slideUp(200);
+        isProfileDropdownOpen = false;
+    }
+    if (isManagementDropdownOpen) {
+        $(managementDropdownMenu).slideUp(200);
+        isManagementDropdownOpen = false;
+    }
 });
