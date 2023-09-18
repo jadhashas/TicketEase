d18916b4049349cddb7fef5b991a50f1e2f140e0
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2023-09-18 13:03:39 +0100

    Upload files (#48)
    * Actor classe
    
    * ajouter les autres classes
    
    * DbContext configuration
    
    * suite configuration DbContext
    
    * Creation et configuration AppDbInitializer
    
    * Create ActorsController
    
    * Suit AllActors
    
    * First view
    
    * Actor view
    
    * view movies
    
    * nav
    
    * ActorsServices
    
    * Services configuration in Program.cs
    
    * Services configuration in Program.cs
    
    * update after cleaning
    
    * update after cleaning
    
    * CRUD
    
    * 404 page edit
    
    * base repository
    
    * configurer BaseRepository
    
    * ProducersService configuration with EntityBaseRepository
    
    * Producers
    
    * Last of producer configuration
    
    * Create cinemas configuration
    
    * cinemas configuration
    
    * debut movies
    
    * movie details
    
    * movie details
    
    * Create movies
    
    * configure dorpDowns
    
    * Partial View for button CreateItem
    
    * tt
    
    * Adding new movie to database
    
    * updating movie
    
    * Searching for movie
    
    * configure models : order and orderItems
    
    * Shoppinrg cart item in folder model
    
    * Setting for the ShoppingCart
    
    * Add methode : AddItemToCart
    
    * add methode : RemoveItemfromCart
    
    * Adding OrdersController file
    
    * Designing shopping cart view and  Configuring sessions & shopping cart as a service
    
    * Suit configuration : OrderCartItem
    
    * configure ViewComponent for display a number of products in the shopping cart
    
    * Adding and configuring OrdersService

---
+++ TicketEase/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using TicketEase.Models;
+
+namespace TicketEase.Data.Services
+{
+	public interface IOrdersService
+	{
+		Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAdresse);
+		Task<List<Order>> GetOrdersByIdUserIsAsync(string userId);
+	}
+}
---
+++ TicketEase/Data/Services/OrdersService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketEase.Models;
+
+namespace TicketEase.Data.Services
+{
+	public class OrdersService : IOrdersService
+	{
+		private readonly AppDbContext _appDbContext;
+        public OrdersService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<List<Order>> GetOrdersByIdUserIsAsync(string userId)
+		{
+			var items = await _appDbContext.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Movie).Where(n => n.UserId == userId).ToListAsync();
+			return items;
+		}
+
+		public async Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAdresse)
+		{
+			var order = new Order()
+			{
+				UserId = userId,
+				Email = userEmailAdresse
+			};
+			await _appDbContext.Orders.AddAsync(order);
+			await _appDbContext.SaveChangesAsync();
+            foreach (var item in items)
+            {
+				var orderItem = new OrderItem()
+				{
+					Amount = item.Amount,
+					MovieId = item.Movie.Id,
+					OrderId = order.Id,
+					Price = item.Movie.Price,
+				};
+				await _appDbContext.OrderItems.AddAsync(orderItem);
+            }
+			await _appDbContext.SaveChangesAsync();
+        }
+	}
+}
