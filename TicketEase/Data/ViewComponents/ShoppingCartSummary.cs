8cfbe307b8421d98a3164bfb976a8576f1f1e950
JAD EL MAWLA HASHAS <HASHAS.JADELMAWLA@ofppt-edu.ma>
2023-09-18 12:42:36 +0100

    configure ViewComponent for display a number of products in the shopping cart

---
+++ TicketEase/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketEase.Data.Cart;
+
+namespace TicketEase.Data.ViewComponents
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _ShoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart ShoppingCart)
+        {
+            _ShoppingCart = ShoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _ShoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
